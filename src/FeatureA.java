9ce2d49c9dc6b3aabc6c37d8f20c076f999a403a
daniel <daniel.windel@unitybox.de>
2023-02-21 17:29:00 +0100

    Add name

--- src/FeatureA.java
+++ src/FeatureA.java
@@ -1,5 +1,5 @@
 public class FeatureA {
     public static void main(String[] args) {
-        System.out.println("Hallo, das ist der Code von Florian");
+        System.out.println("Daniel");
     }
 }
