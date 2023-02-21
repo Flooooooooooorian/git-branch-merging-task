b8cdafc6fab74728279b577f02bd2dcd53ddcd70
timlenny <schlotter.tim@gmail.com>
2023-02-21 17:27:38 +0100

    Change Name in FeauturB to Tim

--- src/FeatureB.java
+++ src/FeatureB.java
@@ -1,6 +1,8 @@
 public class FeatureB {
 
     public static void main(String[] args) {
-        System.out.println("Das ist Feature B von: ");
+
+        System.out.println("Das ist Feature B von: Tim");
+
     }
 }
