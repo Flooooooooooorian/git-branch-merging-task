391db405828f8ca0e6bac4e8555a2cf146737e2d
Ildrassil <schneiderjakob7@googlemail.com>
2023-12-14 12:36:39 +0100

    Add name Jakob

--- src/FeatureA.java
+++ src/FeatureA.java
@@ -1,8 +1,14 @@
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class FeatureA{
 
     public static void main(String[] args) {
         System.out.println("Hallo, das ist der Code von <Name>");
+        List<String> nameSpelling = new ArrayList<>();
+        String name = "Jakob Schneider";
+        nameSpelling = Arrays.stream(name.split("")).toList();
+        nameSpelling.stream().forEach(System.out::println);
     }
 }
