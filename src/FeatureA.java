b7cd75bf48e7f9c35e7d62b3a8f6bccb31607ea5
Yinli <jiangyinli0803@gmail.com>
2025-04-09 22:40:34 +0200

    noch 2 lines hinzufügt

--- src/FeatureA.java
+++ src/FeatureA.java
@@ -4,5 +4,8 @@ public class FeatureA{
 
     public static void main(String[] args) {
         System.out.println("Hallo, das ist der Code von <Yinli Jiang>");
+
+        System.out.println("Das ist ein neuen Feature");
+        System.out.println("Damit kann man wissen, wie Pull Request funktioniert");
     }
 }
